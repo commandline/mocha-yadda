491b0671d4ef9f6bd55c565e0679f03cc50e6017
Thomas Gideon <thomas@webs.com>
2015-08-14 13:25:32 -0400

    Initial port.

---
+++ featureLoader.js
@@ -0,0 +1,33 @@
+var Promise = require('bluebird');
+var http = require('http');
+
+var httpLoad = function(path) {
+    return new Promise(function(resolve, reject) {
+        http.get(window.location.origin + '/base/test/application/features/' + path, function(response) {
+            var data = '';
+
+            response.on('data', function(buf) {
+                data += buf;
+            });
+
+            response.on('end', function() {
+                try {
+                    resolve(data);
+                } catch(err) {
+                    reject(err);
+                }
+            });
+        }).on('error', function(err) {
+            reject(err);
+        });
+    });
+};
+
+module.exports = {
+    http: {
+        load: get
+    },
+    file: {
+        load: undefined
+    }
+};
