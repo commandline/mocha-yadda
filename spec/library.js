73c37824f86818cb16c2cf102326c484da03ff75
Thomas Gideon <thomas@webs.com>
2015-09-01 18:10:32 -0400

    Make sure shared context works as expected.

--- spec/library.js
+++ spec/library.js
@@ -1,4 +1,5 @@
 'use strict';
+var expect = require('chai').expect;
 
 var English = require('yadda').localisation.English;
 
@@ -6,10 +7,13 @@ module.exports = (function() {
     return English.library()
 
     .define('a file to be read', function (next) {
+        this.ctx.someValue = 'something';
         next();
     })
 
     .define('the file can be read', function (next) {
+        expect(this.ctx.someValue, 'should have had some value set from step to step').to.not.be.null;
+        expect(this.ctx).to.have.property('someValue', 'something', 'should have had some value set from step to step');
         next();
     })
 })();
